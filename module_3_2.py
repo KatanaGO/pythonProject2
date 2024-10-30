1da098bb435853067645a1e1140c1f2d0da0f665
KatanaGO <46198582+KatanaGO@users.noreply.github.com>
2024-10-30 22:12:32 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,17 +1,18 @@
 # Функция для отправки email с проверками
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    # Проверка на валидный email (наличие "@" и окончание на ".com", ".ru", ".net")
-    if "@" not in recipient or not (recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
+    # Проверка корректности e-mail отправителя и получателя
+    if ("@" not in sender or not sender.endswith((".com", ".ru", ".net"))) or \
+       ("@" not in recipient or not recipient.endswith((".com", ".ru", ".net"))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    # Проверка на отправку самому себе
     elif sender == recipient:
-        # Проверка на отправку самому себе
         print("Нельзя отправить письмо самому себе!")
+    # Проверка на отправителя по умолчанию
     elif sender == "university.help@gmail.com":
-        # Проверка на отправителя по умолчанию
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    # Обработка нестандартного отправителя
     else:
-        # Сообщение о нестандартном отправителе
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
 
 # Тестовые вызовы функций
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
